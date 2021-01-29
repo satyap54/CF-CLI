cb31f14c431f86ff19b5b82ac26f048d21f1af1a
Satyabrat Panda <46991649+satyap54@users.noreply.github.com>
2021-01-29 16:09:04 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='cf',
+    version='1.0',
+    install_requires=[
+        'Click',
+        'requests',
+    ],
+    entry_points='''
+        [console_scripts]
+        cf = cli:cli
+    ''',    
+)
