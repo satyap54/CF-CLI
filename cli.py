1326aa25c4f39fb347120c5ef930a861c6504ff2
Satyabrat Panda <46991649+satyap54@users.noreply.github.com>
2021-01-29 23:17:18 +0530

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,8 +1,27 @@
-import click
+import click,json
 from set_reminder import set_reminder
+from user_data import user_data
 
-@click.command()
-@click.option('--reminder', '-r', nargs = 0, help = "Set a reminder of next CodeForces Round")
+@click.group()
+def main():
+	pass
 
-def cli(reminder):
+@main.command()
+def remind():
+	""" Set a reminder of next CodeForces Round """
 	set_reminder()
+
+@main.command()
+@click.argument("users", nargs = -1)
+def users(users):
+	""" Gives information about one user """
+	response = user_data(users)
+	print("\tStatus :", response.status_code, response.reason, "\n")
+	data = json.loads(response.text)
+	if(response.ok):
+		for user_ob in data["result"]:
+			print(f'{user_ob["firstName"]} {user_ob["lastName"]} aka {user_ob["handle"]}')
+			print("Current Rating: ",user_ob["rating"])
+			print("Max. Rating: ", user_ob["maxRating"])
+			print("Max. Rank: ", user_ob["maxRank"])
+			print("\n")
