3e2e35ac632d19fdfc892f4f3a641a4d7219acce
Satyabrat Panda <satyabratp.m.2000@gmail.com>
2021-06-25 00:44:01 +0530

    added colors to echos and it works now

--- A.java
+++ A.java
@@ -2,12 +2,80 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
-public class A{
-	 
+public class Slic{
+	
+	static ArrayList<pair> primeFactors(int n){
+		ArrayList<pair> li = new ArrayList<>();
+		int cnt = 0;
+		while(n % 2 == 0){
+			++cnt;
+			n /= 2;
+		}
+
+		if(cnt != 0){
+			li.add(pair.from(2 , cnt));
+			cnt = 0;
+		}
+
+		int lt = (int)Math.sqrt(n);
+		for(int i = 3; i <= lt; i++){
+			if(n % i != 0)
+				continue;
+			while(n % i == 0){
+				++cnt;
+				n /= i;
+			}
+			li.add(pair.from(i, cnt));
+			cnt = 0;
+		}
+
+		if(n > 2)
+			li.add(pair.from(n , 1));
+		return li;
+	} 
+
+	static int nxtPow2(int n){
+		if(n == 1)
+			return 1;
+
+		int cnt = 0, val = 1;
+		while(val < n){
+			val *= 2;
+			++cnt;
+		}
+		return cnt;
+	}
+
+	static boolean isPowerOf2(int n){
+		if(n == 1)
+			return false;
+		int val = 1;
+		while(val < n)
+			val *= 2;
+		return val == n;
+	}
+
 	public static void process(int testNumber){
-		int n = ni(), a = ni(), b = ni();
-		char arr[] = (" " + nln()).toCharArray();
-		pn((arr[a] == arr[b]) ? "0" : "1");	
+		int n = ni();
+		ArrayList<pair> li = primeFactors(n);
+		int val = 1, steps = 0, prev = -1;
+		boolean flag = true, allEqual = true;
+
+		for(pair x : li){
+			val *= x.first;
+			flag = flag && isPowerOf2(x.second);
+			steps = Math.max(steps, nxtPow2(x.second));
+			if(prev != -1)
+				allEqual = allEqual && (prev == x.second);
+			prev = x.second;
+		}
+		
+		if(val == n)
+			steps = 0;
+		else if(!flag || !allEqual)
+			++steps;
+
+		pn(val + " " + steps);
 	}
  
 	static final long mod = (long)1e9+7l;
@@ -72,6 +140,11 @@ class pair implements Comparable<pair> {
         if(this.first != ob.first)
             return this.first - ob.first;
         return this.second - ob.second;
+    }
+
+    @Override
+    public String toString(){
+    	return this.first + " " + this.second;
     }
 
     static public pair from(int f, int s){
--- cli.py
+++ cli.py
@@ -2,9 +2,16 @@ import click,json
 from click import parser
 from tabulate import tabulate
 from set_reminder import set_reminder
-from user_data import user_data
+from user_data import user_data, get_latest_verdict
 from contest_data import contest_data
 from robobrowser import RoboBrowser
+import time
+
+
+_verdict_unexpected = set(("REJECTED", "INPUT_PREPARATION_CRASHED", "CRASHED", "SECURITY_VIOLATED", "PRESENTATION_ERROR",
+						"SKIPPED", "INPUT_PREPARATION_CRASHED"))
+						
+_verdict_end = set(("TIME_LIMIT_EXCEEDED", "MEMORY_LIMIT_EXCEEDED", "IDLENESS_LIMIT_EXCEEDED", "OK", "FAILED", "WRONG_ANSWER"))
 
 
 @click.group()
@@ -51,9 +58,10 @@ def contest(contest):
 	data = json.loads(response.text)
 	
 @main.command()
+@click.argument("contest_id")
 @click.argument("problem_id")
 @click.argument("file_name")
-def submit(problem_id, file_name):
+def submit(contest_id, problem_id, file_name):
 	""" Submit solution file"""
 	
 	# login to codeforces
@@ -66,19 +74,38 @@ def submit(problem_id, file_name):
 
 	res = browser.submit_form(enter_form)
 	enter_form = browser.get_form('enterForm')
-	print("Logged In")
+	click.secho("Logged In", fg="green")
 	
-	browser.open('http://codeforces.com/problemset/submit')
+	browser.open(f'https://codeforces.com/contest/{contest_id}/submit/{problem_id}')
 	submit_form = browser.get_form(class_ = 'submit-form')
-	submit_form['submittedProblemCode'] = problem_id
 	try:
 		submit_form['sourceFile'] = file_name
 	except Exception as e:
-		print("File not found in current dir")
+		click.secho("File not found in current dir", fg="red")
 		return
 	
 	browser.submit_form(submit_form)
 	# check if the submission is done successfully	
 	#print(browser.url)
-	
-	
+	if(browser.url.find("my") == -1):
+		click.secho("Your submission has some problems.\n1)Check if you re-submitted the same solution\n2)Proper class-name for java solution\n3)OJ property", fg='blue')
+		return
+
+	click.secho(f"Submitted {file_name} for problem {problem_id}...", fg="yellow")
+	click.echo("Testing...")
+
+	final_res = None
+	while True:
+		res = get_latest_verdict("KatZura")
+		if(res['verdict'] in _verdict_end):
+			final_res = res
+			break
+		time.sleep(0.5)
+		
+	click.secho("Done!\n", fg="green")
+	#print(tabulate(final_res, headers="keys"))
+	click.secho(final_res["verdict"], fg=("green" if final_res["verdict"] == "OK" else "red"))
+	click.secho(f"Solution Link: https://codeforces.com/contest/{contest_id}/submission/{final_res['id']}", fg="cyan")
+	click.secho(f"Exec. Time: {final_res['time']} ms", fg="cyan")
+	click.secho(f"Memory consm.: {final_res['memory']} KB", fg="cyan")
+	click.secho(f"Ran on {final_res['passedTests']} tests", fg="cyan")
